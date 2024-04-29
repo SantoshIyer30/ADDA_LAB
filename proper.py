9791cf16bd3ab276a12f080b5ed5fb2ba7f61f34
K Santosh Iyer <70837375+SantoshIyer30@users.noreply.github.com>
2024-04-29 18:50:05 +0530

    Update proper.py

--- proper.py
+++ proper.py
@@ -1,19 +1,3 @@
-def calculate_area(length, width):
-  """Calculates the area of a rectangle.
-
-  Args:
-      length (float): The length of the rectangle.
-      width (float): The width of the rectangle.
-
-  Returns:
-      float: The area of the rectangle (length * width).
-  """
-
-
-  return length * width
-
-total_area = calculate_area(5, 3)
-
-
-message = "The total area is: {} square units".format(total_area)
-print(message)
+a = 10
+b = 20
+print (a+b)
